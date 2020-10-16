2191203f1cfcbcfcf9e5d2d159adcdcf89a84bff
danish45007 <danish45007@gmail.com>
2020-10-17 01:51:57 +0530

    Sidebar & Chat component base added

---
+++ client/src/Components/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  "./Chat.css";
+
+function Chat() {
+    return (
+        <div className="app">
+            Chat
+        </div>
+    )
+}
+
+export default Chat
---
+++ client/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCsP2Cio-DDajQa2W1Ahr776FCzNV_hLT0",
+    authDomain: "whatsapp-mern-c8b43.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-c8b43.firebaseio.com",
+    projectId: "whatsapp-mern-c8b43",
+    storageBucket: "whatsapp-mern-c8b43.appspot.com",
+    messagingSenderId: "771385740417",
+    appId: "1:771385740417:web:bf41f6af95bb6cc6939180",
+    measurementId: "G-VBM1297EE1"
+  };
