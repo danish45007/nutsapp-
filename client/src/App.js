26f41747ebb391f61160e0073154736289eb2cae
danish45007 <danish45007@gmail.com>
2020-10-18 06:00:52 +0530

    Backed Pusher integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Chat from './Components/Chat/Chat';
+import { useEffect } from 'react';
+import Pusher from 'pusher-js';
+import axios from './Axios';
 
 function App() {
+
+  const [message, setMessage] = useState([]);
+
+  // getting inital message info from the DB
+  useEffect(() => {
+    axios.get('/v1/api/messages/sync').then((response) => {
+      setMessage(response.data)
+    })
+  }, [])
+
+  // getting the real-time messages from the pusher
+  useEffect(() => {
+    var pusher = new Pusher('6ce87397124b08634e68', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('Message');
+    channel.bind('inserted', function(newMessage) {
+      // alert(JSON.stringify(newMessage));
+      setMessage([...message, newMessage])
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [message])
+  console.log(message);
+
   return (
     <div className="app">
       <div className="app__body">
