a388f7fd11bcf17c3e0244c26b26b4fcb555caa2
danish45007 <danish45007@gmail.com>
2020-10-19 15:07:52 +0530

    emoji_box on chat footer added

--- client/src/Components/ChatFooter/ChatFooter.js
+++ client/src/Components/ChatFooter/ChatFooter.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import "./ChatFooter.css"
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import AttachmentIcon from '@material-ui/icons/Attachment';
+import { Picker } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
 import MicIcon from '@material-ui/icons/Mic';
 import axios from "../../Axios";
+import { IconButton } from '@material-ui/core';
 
 function ChatFooter({ messages }) {
 
+    const [emojiPickerState, SetEmojiPicker] = useState(false);
     const [input, setInput] = useState("")
+    
+    let emojiPicker;
+    if (emojiPickerState) {
+        emojiPicker = (
+            <Picker
+                onSelect={emoji => setInput(input + emoji.native)}
+                style={styles.emojiPicker}
+            />
+        );
+    }
+
+    function triggerPicker(event) {
+        event.preventDefault();
+        SetEmojiPicker(!emojiPickerState);
+      }
+    
+    // posting the inpput message data to server
     const sendMessage = async (e) => {
         e.preventDefault();
         await axios.post('/v1/api/messages/new',{
@@ -18,9 +39,18 @@ function ChatFooter({ messages }) {
         })
         setInput("")
     }
+
     return (
         <div className="chat__footer">
-            <InsertEmoticonIcon />
+        {emojiPicker}
+        <button
+          className="emoji__icon"
+          onClick={triggerPicker}
+        >
+                <IconButton>
+                    <InsertEmoticonIcon />
+                </IconButton>
+        </button>
             <AttachmentIcon />
             <form>
                 <input
@@ -30,12 +60,19 @@ function ChatFooter({ messages }) {
                     type="text"
                 />
                 <button onClick={sendMessage} type="submit">
-                    Send a message
                 </button>
             </form>
             <MicIcon />
         </div>
     )
 }
-
 export default ChatFooter
+
+const styles = {
+    emojiPicker: {
+      position: "absolute",
+      bottom: 10,
+      marginLeft: "200px"
+    }
+  }
+  
